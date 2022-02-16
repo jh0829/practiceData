d94b21669f65a63c994f69a99ccabceaceb70e09
jh <j.hino.0829.lucky2@gmail.com>
2022-02-16 23:16:54 +0900

    setup用SQLを更新

--- setUp.sql
+++ setUp.sql
@@ -1,6 +1,15 @@
+/**
+ データをリセットする時に使う。
+*/
+\encoding UTF8;
+
+-- データを削除
+DROP TABLE testUser;
+
 -- テスト用データ作成 
-CREATE TABLE testData(
+CREATE TABLE testUser(
     id VARCHAR(11) NOT NULL, 
     name VARCHAR(30) NOT NULL ,
-    age integer NOT NULL,
+    age integer,
+    born date,
     PRIMARY KEY (id,name));
